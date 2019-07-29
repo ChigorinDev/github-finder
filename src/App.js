0de09166d6ad524b7001e17da4093af664e55753
Nastasia Serheencko <nastasia.serheencko@yandex.ru>
2019-07-29 18:32:05 +0300

    add input form to find users on github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    const { data } = await Axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      users: data,
+      loading: false
+    });
+  }
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const { data } = await Axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      users: data.items,
+      loading: false
+    });
+    console.log(text);
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <Navbar />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
