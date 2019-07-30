7e0e7232f7e87f78c2f0bee983bd79a2fb9b69e7
Nastasia Serheencko <nastasia.serheencko@yandex.ru>
2019-07-30 18:16:29 +0300

    add routes and user page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import Axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    user: {},
+    repos: [],
+    loading: false,
+    alert: null
   };
 
   async componentDidMount() {
@@ -27,6 +34,7 @@ class App extends Component {
     });
   }
 
+  //get list of users
   searchUsers = async text => {
     this.setState({ loading: true });
 
@@ -43,15 +51,90 @@ class App extends Component {
     console.log(text);
   };
 
+  //Get a single GitHub user
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const { data } = await Axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: data, loading: false });
+  };
+
+  //Get user's repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const { data } = await Axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: data, loading: false });
+  };
+
+  //clear users
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  //set allert
+  setAlert = (msg, type) => {
+    this.setState({
+      alert: { msg, type }
+    });
+
+    //after 3 sec alert goes away
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
+    const { user, users, repos, loading } = this.state;
     return (
-      <div className='app'>
-        <Navbar />
-        <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className='app'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      setAlert={this.setAlert}
+                      showClear={users.length > 0 ? true : false}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
